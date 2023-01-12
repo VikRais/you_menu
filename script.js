7759f62de6d4d1aa328e5b44270712599f8bb478
Nokiabl <nokiabl5cpm@mail.ru>
2023-01-12 10:06:23 +0300

    logo+hero

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggleThemeButton = document.getElementById("toggle-theme-button");
-const root = document.querySelector(':root');
-
-function toggleTheme() {
-  root.classList.toggle('dark-theme');
-}
-
-toggleThemeButton.addEventListener('click', toggleTheme);
